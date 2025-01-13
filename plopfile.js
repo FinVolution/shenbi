f65f7e8d4b7609223031a1d5f9fb57206ed5d0ca
yaoshaohua <eurallyao@163.com>
2025-01-13 17:41:38 +0800

    一键生成组件模板：根据活动名称和组件名称来生成文件，并且在活动名称文件夹不存在时自动创建该文件夹

--- plopfile.js
+++ plopfile.js
@@ -1,60 +1,84 @@
+const commonComponentDir = "common/activity";
+
+const adminAttrsListDefineDir = "admin/src/components/editor/attr/activity";
+const adminAttrsListDefinePath = "admin/src/components/editor/tools/attrsListDefine.js";
+const adminComponentsDefinePath = "admin/src/components/editor/main/componentsDefine.js";
+
+const webComponentDir = "web/components/activity";
+const webServiceDir = "web/service";
+const webStoreDir = "web/store/modules/activity";
+const webComponentsDefinePath = "web/components/common/componentsDefine.js";
+
+function modifyComponentsMap(targetFilePath, componentPath) {
+  return {
+    type: 'modify',
+    path: targetFilePath,
+    pattern: /const\s+componentsMap\s*=\s*\{([^}]+)\}/,
+    template: `const componentsMap = {$1    {{componentName}}: () => import('${componentPath}'),\n}`,
+  };
+}
+
+function modifyAttrMap() {
+  return {
+    type: 'modify',
+    path: adminAttrsListDefinePath,
+    pattern: /(const\s+attrMap\s*=\s*\{)/,
+    template: "$1\n    {{ componentName }}: {\n" +
+      "        componentList: [\n" +
+      "            {\n" +
+      "                name: '{{ componentName }}',\n" +
+      "                attrs: {}\n" +
+      "            }\n" +
+      "        ]\n" +
+      "    },\n"
+  };
+}
+
 module.exports = function (plop) {
   plop.setGenerator("resource", {
     description: "Generating a new activity resource",
     prompts: [
       {
         type: "input",
-        name: "name",
+        name: "activityName",
         message: "Activity name:",
+      },
+      {
+        type: "input",
+        name: "componentName",
+        message: "Component name:",
       },
     ],
     actions: [
       /** 
        * common
        * */
+      // creating activity directory if it doesn't exist
+      {
+        type: 'addMany',
+        destination: `${commonComponentDir}/{{activityName}}`,
+        templateFiles: 'plop-templates/activity-directory/**',
+        base: 'plop-templates/activity-directory',
+      },
       // generating component
       {
         type: "add",
-        path: "common/activity/{{name}}/{{name}}.vue",
+        path: `${commonComponentDir}/{{activityName}}/{{componentName}}.vue`,
         templateFile: "plop-templates/component.vue.hbs",
       },
 
       /**
        * admin
        */
-      // update componentsDefine
-      {
-        type: 'modify',
-        path: 'admin/src/components/editor/main/componentsDefine.js',
-        pattern: /const\s+componentsMap\s*=\s*\{([^}]+)\}/,
-        template: `const componentsMap = {$1    {{name}}: () => import('$root/common/activity/{{name}}/{{name}}.vue'),\n}`,
-      },
       // generating component attribute
       {
         type: "add",
-        path: "admin/src/components/editor/attr/activity/{{name}}/{{name}}.vue",
+        path: `${adminAttrsListDefineDir}/{{activityName}}/{{componentName}}.vue`,
         templateFile: "plop-templates/component-attr.vue.hbs",
       },
-      // update attrsListDefine
-      {
-        type: 'modify',
-        path: 'admin/src/components/editor/tools/attrsListDefine.js',
-        pattern: /const\s+componentsMap\s*=\s*\{([^}]+)\}/,
-        template: `const componentsMap = {$1    {{name}}: () => import('~/editor/attr/activity/{{name}}/{{name}}.vue'),\n}`,
-      },
-      {
-        type: 'modify',
-        path: 'admin/src/components/editor/tools/attrsListDefine.js',
-        pattern: /(const\s+attrMap\s*=\s*\{)/,
-        template: "$1\n    {{ name }}: {\n" +
-                  "        componentList: [\n" +
-                  "            {\n" +
-                  "                name: '{{ name }}',\n" +
-                  "                attrs: {}\n" +
-                  "            }\n" +
-                  "        ]\n" +
-                  "    },\n"
-      },
+      modifyAttrMap(),
+      modifyComponentsMap(adminAttrsListDefinePath, `~/editor/attr/activity/{{activityName}}/{{componentName}}.vue`),
+      modifyComponentsMap(adminComponentsDefinePath, `$root/${commonComponentDir}/{{activityName}}/{{componentName}}.vue`),
 
       /**
        * web
@@ -62,28 +86,24 @@ module.exports = function (plop) {
       // generating component
       {
         type: "add",
-        path: "web/components/activity/{{name}}/{{name}}.vue",
+        path: `${webComponentDir}/{{activityName}}/{{componentName}}.vue`,
         templateFile: "plop-templates/component-web.vue.hbs",
-      },
-      // update componentsDefine
-      {
-        type: 'modify',
-        path: 'web/components/common/componentsDefine.js',
-        pattern: /const\s+componentsMap\s*=\s*\{([^}]+)\}/,
-        template: `const componentsMap = {$1    {{name}}: () => import('@/components/activity/{{name}}/{{name}}.vue'),\n}`,
       },
       // generating service
       {
         type: "add",
-        path: "web/service/{{name}}.ts",
+        path: `${webServiceDir}/{{activityName}}.ts`,
         templateFile: "plop-templates/component-service.ts.hbs",
+        skipIfExists: true,
       },
       // generating store
       {
         type: "add",
-        path: "web/store/modules/activity/{{name}}/index.ts",
+        path: `${webStoreDir}/{{activityName}}/index.ts`,
         templateFile: "plop-templates/component-store.ts.hbs",
+        skipIfExists: true,
       },
+      modifyComponentsMap(webComponentsDefinePath, `@/components/activity/{{activityName}}/{{componentName}}.vue`),
     ],
   });
 };
